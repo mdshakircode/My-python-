ddffacdd87b6146b8d977b507168d6f16cf46d41
mdshakircode <mohammedshakir2193@gmail.com>
2025-11-12 18:10:30 +0530

    Create User details.py

---
+++ User details.py
@@ -0,0 +1,7 @@
+# user details
+def userdetails():
+  name=input("enter your name")
+  age=int(input("enter your age "))
+  rollnumber=int(input("enter your roll number"))
+  return name,age,rollnumber
+userdetails()
